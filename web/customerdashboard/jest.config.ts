4a7c77091c33b2c1dfa976f1bfabdf13e23ce4de
varun325 <sharmavarun1300@gmail.com>
2024-03-09 20:49:21 +0530

    Fixed coverage from arguments to take coverage from all ts and tsx files

--- web/customerdashboard/jest.config.ts
+++ web/customerdashboard/jest.config.ts
@@ -1,13 +1,13 @@
 export default {
     preset: "ts-jest",
     testEnvironment: "jest-environment-jsdom",
-    moduleFileExtensions: ["js", "json", "ts"],
+    moduleFileExtensions: ["js", "json", "ts", "tsx"],
     roots: ["src"],
     testRegex: ".*\\.spec\\.ts$",
     transform: {
         "^.+\\.(t|j)s$": "ts-jest",
     },
-    collectCoverageFrom: ["**/*.(t|j)s"],
+    collectCoverageFrom: ["**/*.(t|j)s", "**/*.(t|j)sx"],
     coverageDirectory: "../coverage",
     moduleDirectories: ["node_modules", "src", __dirname],
     moduleNameMapper: {
